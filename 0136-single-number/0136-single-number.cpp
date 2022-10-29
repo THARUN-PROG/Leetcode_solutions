42e253db11eb30d0b1e02cb45e901174617477bb
THARUN S <81684759+THARUN-PROG@users.noreply.github.com>
2022-10-29 20:15:50 +0530

    Time: 39 ms (62.42%), Space: 17 MB (52.83%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& n) {
-        int ans = 0;
-        for(auto x: n)
-        ans^=x;
-        return ans;
-    }
-};
+    int singleNumber(vector<int>& a) {
+        int n = a.size();
+        sort(a.begin(),a.end());
+        for(int i = 0;i<n-1;i+=2){
+            if(a[i]!=a[i+1]) return a[i];
+
+        }
+                    
+            return a[n-1];
         
+       
+        
+    }
+     
+};
