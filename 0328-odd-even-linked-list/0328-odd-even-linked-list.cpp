bb341d5fc24704ed175c097e0c918d1801c334f8
THARUN S <81684759+THARUN-PROG@users.noreply.github.com>
2022-11-01 19:07:07 +0530

    Time: 25 ms (53.48%), Space: 10.5 MB (74.63%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,16 +11,40 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(head == NULL) return NULL;
-        ListNode * odd = head, * even = head->next, * evenHead = even;
-        while(even!=NULL && even->next!=NULL){
-            odd->next = even->next;
-            odd=odd->next;
-            even->next = odd->next;
-            even=even->next;
-        }
-        odd->next = evenHead;
+        typedef struct ListNode node;
+        node * firsteven, * lasteven, *firstodd, *lastodd, *th;
+        firsteven = lasteven = firstodd = lastodd = NULL;
+        th = head;
+        int flag = 0;
+        if(head==NULL) return NULL;
+        if(head->next == NULL) return head;
+        while(th != NULL){
+            if(flag == 1){
+                if(firsteven == NULL)
+                    firsteven = lasteven = th;
+                else
+                {
+                    lasteven -> next = th;
+                    lasteven = th;
+                }
+            }
+            
+            else
+            {
+                if(firstodd == NULL)
+                    firstodd = lastodd = th;
+                else
+                {
+                    lastodd -> next = th;
+                    lastodd = th;
+                }
+            }
+            flag=!flag;
+            th = th-> next;
+    }
+       head = firstodd;
+       lastodd -> next = firsteven;
+       lasteven -> next = NULL;
         return head;
-        
     }
 };
